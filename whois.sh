aeb2a2a689e8b5822313c807f77c81d529273144
Aaron Dhiman <aaron.dhiman@gmail.com>
2019-05-19 17:31:00 -0400

    updated greps

--- whois.sh
+++ whois.sh
@@ -4,5 +4,5 @@ touch netput.txt
 netstat -antup  | cut -d":" -f2 | sed "/^ *$/d" | awk '{print $2}'| grep '^[1-9]' > IPs.txt 
 while read ip; do
 	echo -e "\n\n***********************\n$ip\n***********************" >> netput.txt
-	whois $ip | grep 'OrgName\|City\|State\|Coun' >> netput.txt
+	whois $ip | grep 'OrgName\|City\|State\|ountry\|role\|address' >> netput.txt
 done < IPs.txt
