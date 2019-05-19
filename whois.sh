0a8d23a982b74304885ff7dbaf077e066f74ab81
Aaron Dhiman <aaron.dhiman@gmail.com>
2019-05-19 17:22:49 -0400

    added 'p' flag to netstat

--- whois.sh
+++ whois.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 touch netput.txt
-netstat -antu  | cut -d":" -f2 | sed "/^ *$/d" | awk '{print $2}'| grep '^[1-9]' > IPs.txt 
+netstat -antup  | cut -d":" -f2 | sed "/^ *$/d" | awk '{print $2}'| grep '^[1-9]' > IPs.txt 
 while read ip; do
 	echo -e "\n\n***********************\n$ip\n***********************" >> netput.txt
 	whois $ip | grep 'OrgName\|City\|State\|Coun' >> netput.txt
