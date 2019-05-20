15a9c32087550207b0b6d46960d6f3dfe8beb565
Aaron Dhiman <aaron.dhiman@gmail.com>
2019-05-20 16:28:30 -0400

    added Reverse DNS lookup

--- whois.sh
+++ whois.sh
@@ -7,10 +7,11 @@ netstat -antup | awk '{print $5, $6, $7}' | grep '^[1-9]' > netstat.tmp
 
 while read myline; do
 	IP=`echo $myline | cut -d":" -f1`
+	DOMAIN=`nslookup $IP | grep name | cut -d "=" -f2`
 	PROC=`echo $myline | awk '{print $3}'`
 	STATE=`echo $myline | awk '{print $2}'`
-	echo -e "\n\n***********************\n$IP       $STATE \n$PROC" >> netput.txt
-	whois $IP | grep 'OrgName\|City\|State\|ountry\|role' >> netput.txt
+	echo -e "\n\n***********************\n$IP       $STATE\n  $DOMAIN\nproc = $PROC" >> netput.txt
+	whois $IP | grep 'OrgName\|City\|State\|ountry\|role\|OrgTechName' >> netput.txt
 	echo -e "***********************" >> netput.txt
 done < netstat.tmp
 
