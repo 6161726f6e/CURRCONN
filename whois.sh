200419da41dd4bdfd917f446348c313436d79af5
Aaron <aaron.dhiman@gmail.com>
2019-05-19 23:41:10 -0400

    added conn status

--- whois.sh
+++ whois.sh
@@ -3,11 +3,13 @@
 touch netput.txt
 touch netstat.tmp
 netstat -antup | awk '{print $5, $6, $7}' | grep '^[1-9]' > netstat.tmp
+#cat netstat.tmp
 
 while read myline; do
 	IP=`echo $myline | cut -d":" -f1`
 	PROC=`echo $myline | awk '{print $3}'`
-	echo -e "\n\n***********************\n$IP\n$PROC" >> netput.txt
+	STATE=`echo $myline | awk '{print $2}'`
+	echo -e "\n\n***********************\n$IP       $STATE \n$PROC" >> netput.txt
 	whois $IP | grep 'OrgName\|City\|State\|ountry\|role' >> netput.txt
 	echo -e "***********************" >> netput.txt
 done < netstat.tmp
