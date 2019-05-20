2f715739ce1b3605b7b9f629e8fd691052afe9c0
Aaron Dhiman <aaron.dhiman@gmail.com>
2019-05-20 19:28:23 -0400

    added data and status messages.  better formatting.

--- whois.sh
+++ whois.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+#
+ 
 touch netput.txt
 touch netstat.tmp
 netstat -antup | awk '{print $5, $6, $7}' | grep '^[1-9]' > netstat.tmp
@@ -7,13 +8,17 @@ netstat -antup | awk '{print $5, $6, $7}' | grep '^[1-9]' > netstat.tmp
 
 while read myline; do
 	IP=`echo $myline | cut -d":" -f1`
+	IPP=`echo $myline | awk '{print $1}'`
+	DPORT=`echo $myline | cut -d":" -f2`
 	DOMAIN=`nslookup $IP | grep name | cut -d "=" -f2`
-	PROC=`echo $myline | awk '{print $3}'`
+	PROC=`echo $myline | awk '{print $3}' | cut -d "-" -f2`
 	STATE=`echo $myline | awk '{print $2}'`
-	echo -e "\n\n***********************\n$IP       $STATE\n  $DOMAIN\nproc = $PROC" >> netput.txt
+	echo "looking up $IP"
+	echo -e "\n\n**********************************************\nproc = $PROC\n$IPP       $STATE\n $DOMAIN\n   \n" >> netput.txt
 	whois $IP | grep 'OrgName\|City\|State\|ountry\|role\|OrgTechName' >> netput.txt
-	echo -e "***********************" >> netput.txt
+	echo -e "*********************************************" >> netput.txt
 done < netstat.tmp
 
+echo -e "------------------------\nRESULTS IN netput.txt\n------------------------\n"
 rm *.tmp
 
