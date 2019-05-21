73709d5e372cb60d1b01a49b9288c8ba7119ff66
Aaron Dhiman <aaron.dhiman@gmail.com>
2019-05-21 17:35:42 -0400

    formatting, comments

--- whois.sh
+++ whois.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+#Author: Aaron Dhiman
 #
+#This script takes a snapshot of current network connections.
+#It then prints out useful inforation about all of the connections, 
+#such as who owns the remote computer, what application is using each
+#connection, what ports are being connected, domain names associated
+#with the remote peer, etc.  The results are output to netput.txt
+#####################################################################
  
 touch netput.txt
 touch netstat.tmp
@@ -13,8 +20,9 @@ while read myline; do
 	DOMAIN=`nslookup $IP | grep name | cut -d "=" -f2`
 	PROC=`echo $myline | awk '{print $3}' | cut -d "-" -f2`
 	STATE=`echo $myline | awk '{print $2}'`
+
 	echo "looking up $IP"
-	echo -e "\n\n**********************************************\nproc = $PROC\n$IPP       $STATE\n $DOMAIN\n   \n" >> netput.txt
+	echo -e "\n\n**********************************************\nproc = $PROC\n$IPP       $STATE\n$DOMAIN\n" >> netput.txt
 	whois $IP | grep 'OrgName\|City\|State\|ountry\|role\|OrgTechName' >> netput.txt
 	echo -e "*********************************************" >> netput.txt
 done < netstat.tmp
