a9cab34ec4e56bd1e65853af7f637421466364b8
Aaron <aaron.dhiman@gmail.com>
2019-05-29 23:48:46 -0400

    needed to adjust check for valid dest IP after adding in source port processing

--- whois.sh
+++ whois.sh
@@ -10,14 +10,14 @@
  
 touch netput.txt
 touch netstat.tmp
-netstat -antup | awk '{print $4, $5, $6, $7}' | grep '^[1-9]' > netstat.tmp
+netstat -antup | awk '{print $5, $4, $6, $7}' | grep '^[1-9]' > netstat.tmp
 #cat netstat.tmp
 
 while read myline; do
-	IP=`echo $myline | awk '{print $2}' | cut -d":" -f1`
-	IPP=`echo $myline | awk '{print $2}'`
-	DPORT=`echo $myline | awk '{print $2}' | cut -d":" -f2`
-	LPORT=`echo $myline | awk '{print $1}' | cut -d":" -f2`
+	IP=`echo $myline | awk '{print $1}' | cut -d":" -f1`
+	IPP=`echo $myline | awk '{print $1}'`
+	DPORT=`echo $myline | awk '{print $1}' | cut -d":" -f2`
+	LPORT=`echo $myline | awk '{print $2}' | cut -d":" -f2`
 	DOMAIN=`nslookup $IP | grep name | cut -d "=" -f2`
 	PROC=`echo $myline | awk '{print $4}' | cut -d "/" -f2`
 	STATE=`echo $myline | awk '{print $3}'`
