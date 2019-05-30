a4a361000c772b8b0befaee04bd4f440b9bd353c
Aaron <aaron.dhiman@gmail.com>
2019-05-29 23:42:44 -0400

    replaced PID with DestPort for proc output, formatting

--- whois.sh
+++ whois.sh
@@ -10,19 +10,20 @@
  
 touch netput.txt
 touch netstat.tmp
-netstat -antup | awk '{print $5, $6, $7}' | grep '^[1-9]' > netstat.tmp
+netstat -antup | awk '{print $4, $5, $6, $7}' | grep '^[1-9]' > netstat.tmp
 #cat netstat.tmp
 
 while read myline; do
-	IP=`echo $myline | cut -d":" -f1`
-	IPP=`echo $myline | awk '{print $1}'`
-	DPORT=`echo $myline | cut -d":" -f2`
+	IP=`echo $myline | awk '{print $2}' | cut -d":" -f1`
+	IPP=`echo $myline | awk '{print $2}'`
+	DPORT=`echo $myline | awk '{print $2}' | cut -d":" -f2`
+	LPORT=`echo $myline | awk '{print $1}' | cut -d":" -f2`
 	DOMAIN=`nslookup $IP | grep name | cut -d "=" -f2`
-	PROC=`echo $myline | awk '{print $3}' | cut -d "-" -f2`
-	STATE=`echo $myline | awk '{print $2}'`
+	PROC=`echo $myline | awk '{print $4}' | cut -d "/" -f2`
+	STATE=`echo $myline | awk '{print $3}'`
 
 	echo "looking up $IP"
-	echo -e "\n\n**********************************************\nproc = $PROC\n$IPP       $STATE\n$DOMAIN\n" >> netput.txt
+	echo -e "\n\n**********************************************\nproc = $PROC:$LPORT\ndest = $IPP       $STATE\n $DOMAIN\n   \n" >> netput.txt
 	whois $IP | grep 'OrgName\|City\|State\|ountry\|role\|OrgTechName' >> netput.txt
 	echo -e "*********************************************" >> netput.txt
 done < netstat.tmp
